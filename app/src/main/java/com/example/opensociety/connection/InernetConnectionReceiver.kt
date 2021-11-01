2e52871ce8d4d8c2fff82f46dbd28a6f132ae18e
Andrei.Moiseenko <AndreAKM@yandex.ru>
2021-11-01 13:01:45 +0100

    Up target SDK to Android 9

---
+++ app/src/main/java/com/example/opensociety/connection/InernetConnectionReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.opensociety.connection
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class InernetConnectionReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("InernetConnectionReceiver.onReceive() is not implemented")
+    }
+}
