04d3eb819c92457e6133a9c642c7da63384c226a
Andrei.Moiseenko <AndreAKM@yandex.ru>
2021-11-02 14:53:10 +0100

    Feature
    add reciver to start the serveice when the system has been started.

---
+++ app/src/main/java/com/example/opensociety/connection/FoneServiceReceiver.kt
@@ -0,0 +1,26 @@
+package com.example.opensociety.connection
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import com.example.opensociety.db.Contacts
+
+class FoneServiceReceiver : BroadcastReceiver() {
+    val TAG = "FoneServiceReceiver"
+    
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "catch ${intent.action.toString()}")
+        val intentService = ServiceCommandBuilder(context)
+            .setCommand(ServiceCommandBuilder.Command.START).build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intentService)
+        } else {
+            context.startService(intentService)
+        }
+        Contacts(context).updateIP()
+    }
+}
